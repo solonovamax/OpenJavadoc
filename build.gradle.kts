ee5e9ae923848c11420eb1d0a18662dd1e716552
solonovamax <solonovamax@12oclockpoint.com>
2022-02-13 14:45:23 -0500

    Add kotlin jpa compiler plugin for spring support

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
+    kotlin("plugin.jpa") version "1.6.10"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.springframework.boot") version "2.6.3"
 }
